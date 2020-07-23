1e635d07d591c168ff7cacb067791305f4c445d1
bertanbee <bertan1707@gmail.com>
2020-07-23 14:22:05 -0300

    message-box top done

--- discord/src/components/header/Header.js
+++ discord/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaPhone, FaVideo, FaThumbtack, FaPlus, FaSearch, FaInbox, FaQuestionCircle } from 'react-icons/fa';
 
 import './styles.css';
 
@@ -7,15 +8,25 @@ function Header () {
         <div className="box-head">
             <div className="left-right"> 
                 <div className="row">
-                   <h3>@</h3>
-                    <p>Name</p>
+                   <h3 id="arroba">@</h3>
+                    <p id="name-top">Name</p>
+                    <div className="activity"></div>
                 </div>
                     
                 <div className="row">
                     <div className="buttons">
-
+                        <FaPhone className="icon-top"></FaPhone>
+                        <FaVideo className="icon-top"></FaVideo>
+                        <FaThumbtack className="icon-top"></FaThumbtack>
+                        <FaPlus className="icon-top"></FaPlus>
+                        <div className="search-box">
+                            <input placeholder="Search"></input>
+                            <FaSearch id="search-icon"></FaSearch>
+                        </div>
+                        <FaInbox className="icon-top"></FaInbox>       
+                        <FaQuestionCircle className="icon-top"></FaQuestionCircle>                 
                     </div>
-                </div>
+                </div> 
             </div>
             
         </div>
